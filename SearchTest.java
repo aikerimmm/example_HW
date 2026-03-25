88bddabfea495a58fec24c63b8ab50a87035c47b
adastanious <dastanaidarov1@gmail.com>
2026-03-25 13:45:34 +0600

    HW_1_Done

---
+++ SearchTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTest {
+    @Test
+    void lockedUserTest(){
+        open("https://www.saucedemo.com/");
+        $("#user-name").setValue("locked_out_user");
+        $("[data-test=password]").setValue("secret_sauce");
+        $(".submit-button").click();
+        $("[data-test=error]").shouldHave(text("Epic sadface: Sorry, this user has been locked out."));
+        sleep(2000);
+    }
+}
